dcd62f8af653dac883cb50caa7017ed01396e34f
YangWang0124 <y6228725@gmail.com>
2025-12-19 12:28:35 +1300

    Add home redirect route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
         <Route path="/login" element={<Login />} />
 
         <Route
